a85af8c215f0f193a8e051a5818b8b103e0ab6fe
Alexander Sviridov <sviridov.vmi@gmail.com>
2021-09-09 14:03:57 +0500

    Initial prototype

---
+++ src/index.js
@@ -0,0 +1,94 @@
+import './styles.css';
+
+const $ = window.jQuery;
+
+let recommendationForm;
+let atrialFibrillationCheckbox;
+let prostheticAorticValve;
+let prostheticMitralValve;
+let MHOValue;
+let getRecommendation;
+let recommendationContainer;
+
+function initSelectors() {
+  recommendationForm = $('#recommendationForm');
+  atrialFibrillationCheckbox = $('#atrialFibrillation');
+  prostheticAorticValve = $('#prostheticAorticValve');
+  prostheticMitralValve = $('#prostheticMitralValve');
+  MHOValue = $('#MHOValue');
+  getRecommendation = $('#getRecommendation');
+  recommendationContainer = $('#recommendationContainer');
+}
+
+function initEventListeners() {
+  atrialFibrillationCheckbox.on("change", checkAbilityToRequestRecommendations);
+  prostheticAorticValve.on("change", checkAbilityToRequestRecommendations);
+  prostheticMitralValve.on("change", checkAbilityToRequestRecommendations);
+  MHOValue.on("input", checkAbilityToRequestRecommendations);
+
+  recommendationForm.on("submit", onRecommendationRequested);
+}
+
+function checkAbilityToRequestRecommendations() {
+  const reasonIsSelected = atrialFibrillationCheckbox.is(':checked') ||
+    prostheticAorticValve.is(':checked') ||
+    prostheticMitralValve.is(':checked');
+
+  const MHOValueText = Number(MHOValue.val().replace(',', '.'));
+  const MHOValueIsProvided = !!MHOValueText;
+
+  getRecommendation.prop('disabled', !(reasonIsSelected && MHOValueIsProvided));
+  recommendationContainer.html('');
+}
+
+function onRecommendationRequested(event) {
+  event.preventDefault();
+  
+  const reasons = [];
+
+  if (atrialFibrillationCheckbox.is(':checked')) {
+    reasons.push('фибрилляций предсердий');
+  }
+
+  if (prostheticAorticValve.is(':checked')) {
+    reasons.push('протезированного аортального клапана');
+  }
+
+  if (prostheticMitralValve.is(':checked')) {
+    reasons.push('протезированного митрального клапана');
+  }
+
+  const reason = reasons.length == 1
+    ? reasons[0]
+    : reasons.length == 2
+      ? reasons.join(' и ')
+      : `${reasons[0]}, ${reasons[1]} и ${reasons[2]}`;
+      
+
+  const prostheticMitralValveIsSelected = prostheticMitralValve.is(':checked');
+  const MHOValueProvided = Number(MHOValue.val().replace(',', '.'));
+  const [lowerMHOBound, upperMHOBound] = prostheticMitralValveIsSelected ? [2.5, 3.5] : [2.0, 3.0];
+
+  const recommendation = MHOValueProvided < lowerMHOBound
+    ? 'ниже целевого, из-за чего повышен риск образования кровяных сгустков. Рекомендуется <b>повысить</b> дозу Варфарина'
+    : MHOValueProvided > upperMHOBound
+      ? 'выше целевого, из-за чего повышен риск кровотечений. Рекомендуется <b>снизить</b> дозу Варфарина'
+      : 'внутри целевого диапазона';
+
+  const text = `
+    <div class="form_group form_group__vertical">
+      <label class="form_group_label">Рекомендация:</label>
+      <p class="form_group_paragraph">
+        Целевые значения MHO в случае ${reason}: <b>${lowerMHOBound} - ${upperMHOBound}</b>.<br /><br />
+        Уровень MHO равный <b>${MHOValueProvided}</b> ${recommendation}.
+      </p>
+    </div>
+  `;
+
+  recommendationContainer.html(text);
+}
+
+$(function () {
+  initSelectors();
+  initEventListeners();
+});
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const { basename } = require('path')
+
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'production',
+  performance: {
+    hints: false
+  },
+  output: {
+    path: __dirname + '/dist',
+    filename: 'index.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin(),
+    new FaviconsWebpackPlugin({
+      cache: true,
+      logo: './src/assets/heart.svg',
+      inject: htmlPlugin => 
+        basename(htmlPlugin.options.filename) === 'index.html',
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: "./src/assets", to: "assets" },
+      ],
+    }),
+  ]
+}
